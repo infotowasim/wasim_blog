e1ec11c013886c1931b39138d6fa9ac7e5e77e8e
infotowasim <infotowasim@gmail.com>
2024-06-15 17:27:20 +0530

    1st commit

---
+++ src/main/java/com/wasim_blog/WasimBlogApplication.java
@@ -0,0 +1,14 @@
+package com.wasim_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasimBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasimBlogApplication.class, args);
+		System.out.println("wasim");
+	}
+
+}
---
+++ src/test/java/com/wasim_blog/WasimBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasim_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasimBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
